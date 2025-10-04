901dc683b1b153884f42565e181feda941ad4316
Ace7836-hash <aace7836@gmail.com>
2025-10-05 06:33:02 +0800

    Update SYNDICATE%MAIN%CHEAT((SYNDICATE MAIN CHEAT)).lua

--- SYNDICATE%MAIN%CHEAT((SYNDICATE MAIN CHEAT)).lua
+++ SYNDICATE%MAIN%CHEAT((SYNDICATE MAIN CHEAT)).lua
@@ -606,3 +606,49 @@ Callback = function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpySource.lua"))()
 end
 })
+	else
+		btn.Text = "❌ Invalid key.Try again"
+		task.delay(1.5,function()
+			btn.Text = "ENTER KEY"
+		end)
+	end
+end)
+
+local UserInputService = game:GetService("UserInputService")
+local dragging, dragInput, dragStart, startPos
+
+local function update(input)
+	local delta = input.Position - dragStart
+	frame.Position = UDim2.new(
+		startPos.X.Scale,
+		startPos.X.Offset + delta.X,
+		startPos.Y.Scale,
+		startPos.Y.Offset + delta.Y
+	)
+end
+
+frame.InputBegan:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+		dragging = true
+		dragStart = input.Position
+		startPos = frame.Position
+
+		input.Changed:Connect(function()
+			if input.UserInputState == Enum.UserInputState.End then
+				dragging = false
+			end
+		end)
+	end
+end)
+
+frame.InputChanged:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+		dragInput = input
+	end
+end)
+
+UserInputService.InputChanged:Connect(function(input)
+	if input == dragInput and dragging then
+		update(input)
+	end
+end)
